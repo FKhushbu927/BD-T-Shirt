c0b3a53481465b6857cf078d96b59f73c625028b
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-11-04 15:13:30 +0600

    updated

--- src/components/Header/Cart/Cart.jsx
+++ src/components/Header/Cart/Cart.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Cart = ({cart,  handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some products</p>    
+    }else{
+        message = <div>
+            <h3>Thanks for adding products</h3>
+        </div>
+    }
     return (
         <div>
             <h2>Order summary:{cart.length}</h2>
+            {cart.length > 2 ? <span>Want to Buy more!!</span> : <span>Having Low budget?</span>}
+            {message}
 
             {
                 cart.map(eachTShirt => <p
